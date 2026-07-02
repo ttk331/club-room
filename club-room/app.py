59b8e52510d9358d95c606c0931a46f86e940b48
ttk331 <qttkpdesu@gmail.com>
2026-07-02 13:16:55 +0900

    Update app.py

--- club-room/app.py
+++ club-room/app.py
@@ -356,6 +356,17 @@ def save_comment():
     })
 
     return redirect("/admin")
+
+@app.route("/delete_comment", methods=["POST"])
+def delete_comment():
+
+    date = request.form.get("date")
+
+    comments_collection.delete_many({
+        "日付": date
+    })
+
+    return redirect("/admin")
     
 @app.route("/health")
 def health():
